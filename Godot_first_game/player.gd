fd98f6193e7c51bf02270fe6b02c5fd6e9f2a247
Rishi Aluri <rishialuri@gmail.com>
2024-06-06 22:43:01 +0530

    Second commit

--- Godot_first_game/player.gd
+++ Godot_first_game/player.gd
@@ -1,11 +1,12 @@
 extends Area2D
+signal hit
 
 @export var speed = 400
 var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
+	#hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -39,3 +40,15 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.flip_v = false
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
